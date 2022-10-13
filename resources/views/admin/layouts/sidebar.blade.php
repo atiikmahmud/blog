582a5a4b508efa5559736d41f07aa0b44448fdee
Atik Mahmud <atikmahmud.cse@gmail.com>
2022-10-13 10:28:03 +0600

    Admin panel contact message and sidebar active link updated

--- resources/views/admin/layouts/sidebar.blade.php
+++ resources/views/admin/layouts/sidebar.blade.php
@@ -12,7 +12,7 @@
     <hr class="sidebar-divider my-0">
 
     <!-- Nav Item - Dashboard -->
-    <li class="nav-item active">
+    <li class="nav-item @if($title == 'Dashboard') active @endif">
         <a class="nav-link" href="{{ route('admin.dashboard') }}">
             <i class="fas fa-fw fa-tachometer-alt"></i>
             <span>Dashboard</span></a>
@@ -29,22 +29,28 @@
     <!-- Nav Item - Pages Collapse Menu -->
     
     <!-- Nav Item - Charts -->
-    <li class="nav-item">
+    <li class="nav-item  @if($title == 'Posts') active @endif">
         <a class="nav-link" href="{{ route('admin.posts') }}">
             <i class="fas fa-file-powerpoint"></i>
             <span>Posts</span></a>
     </li>
 
-    <li class="nav-item">
+    <li class="nav-item  @if($title == 'Add Post') active @endif">
         <a class="nav-link" href="{{ route('admin.add.post') }}">
             <i class="fas fa-file-signature"></i>
             <span>Add Posts</span></a>
     </li>
 
-    <li class="nav-item">
+    <li class="nav-item  @if($title == 'Comments') active @endif">
         <a class="nav-link" href="{{ route('admin.comments') }}">
             <i class="fas fa-comment-alt"></i>
             <span>Comments</span></a>
+    </li>
+
+    <li class="nav-item  @if($title == 'Messages') active @endif">
+        <a class="nav-link" href="{{ route('admin.messages') }}">
+            <i class="fas fa-envelope"></i>
+            <span>Messages</span></a>
     </li>
 
     <!-- Divider -->
@@ -58,13 +64,13 @@
     <!-- Nav Item - Pages Collapse Menu -->
     
     <!-- Nav Item - Charts -->
-    <li class="nav-item">
+    <li class="nav-item  @if($title == 'Users') active @endif">
         <a class="nav-link" href="{{ route('admin.users') }}">
             <i class="fas fa-users"></i>
             <span>Users</span></a>
     </li>
 
-    <li class="nav-item">
+    <li class="nav-item  @if($title == 'Add User') active @endif">
         <a class="nav-link" href="{{ route('admin.add.user') }}">
             <i class="fas fa-user-plus"></i>
             <span>Add User</span></a>
@@ -81,13 +87,13 @@
     <!-- Nav Item - Pages Collapse Menu -->
     
     <!-- Nav Item - Charts -->
-    <li class="nav-item">
+    <li class="nav-item  @if($title == 'Admin Users') active @endif">
         <a class="nav-link" href="{{ route('admin.admin.users') }}">
             <i class="fas fa-user-shield"></i>
             <span>Admin Users</span></a>
     </li>
 
-    <li class="nav-item">
+    <li class="nav-item  @if($title == 'Add Admin User') active @endif">
         <a class="nav-link" href="{{ route('add.admin.user') }}">
             <i class="fas fa-user-plus"></i>
             <span>Add Admin User</span></a>
