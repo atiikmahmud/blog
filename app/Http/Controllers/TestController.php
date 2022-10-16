8940b35b49b98bc5dcc14d949d92d438768cfb10
Atik Mahmud <atikmahmud.cse@gmail.com>
2022-10-16 23:38:28 +0600

    new user panel home page almost complete

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -15,7 +15,13 @@ class TestController extends Controller
         $breakingNews = Post::where('status',1)->where('breaking_news', 1)->orderBy('id', 'desc')->latest()->take(3)->get();
         $featurePosts = Post::with('categories')->where('feature_news', 1)->where('status',1)->orderBy('id', 'desc')->latest()->take(5)->get();
         $all_posts = Post::with('categories')->where('status', 1)->orderBy('id', 'desc')->latest()->take(14)->get();
+
+        $trandingPosts = Post::with('categories')->where('tranding_news', 1)->where('status',1)->orderBy('id', 'desc')->latest()->take(5)->get();
+
+        $tags = Post::select('tag')->distinct()->where('status', 1)->get();
         
-        return view('test.index', compact('sliders', 'latestPosts', 'breakingNews', 'featurePosts', 'all_posts'));
+        return view('test.index', compact('sliders', 'latestPosts', 'breakingNews', 'featurePosts', 'all_posts', 'tags', 'trandingPosts'));
+
+
     }
 }
--- resources/views/test/layouts/tags.blade.php
+++ resources/views/test/layouts/tags.blade.php
@@ -4,16 +4,9 @@
     </div>
     <div class="bg-white border border-top-0 p-3">
         <div class="d-flex flex-wrap m-n1">
-            <a href="" class="btn btn-sm btn-outline-secondary m-1">Politics</a>
-            <a href="" class="btn btn-sm btn-outline-secondary m-1">Business</a>
-            <a href="" class="btn btn-sm btn-outline-secondary m-1">Corporate</a>
-            <a href="" class="btn btn-sm btn-outline-secondary m-1">Business</a>
-            <a href="" class="btn btn-sm btn-outline-secondary m-1">Health</a>
-            <a href="" class="btn btn-sm btn-outline-secondary m-1">Education</a>
-            <a href="" class="btn btn-sm btn-outline-secondary m-1">Science</a>
-            <a href="" class="btn btn-sm btn-outline-secondary m-1">Business</a>
-            <a href="" class="btn btn-sm btn-outline-secondary m-1">Foods</a>
-            <a href="" class="btn btn-sm btn-outline-secondary m-1">Travel</a>
+            @foreach($tags as $tag)
+            <a href="" class="btn btn-sm btn-outline-secondary m-1">{{ $tag->tag }}</a>
+            @endforeach
         </div>
     </div>
 </div>
