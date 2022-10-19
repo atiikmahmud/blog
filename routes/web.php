395c34fe6568ca6de458ba2e40e54f83f3466dce
Atik Mahmud <atikmahmud.cse@gmail.com>
2022-10-19 21:53:58 +0600

    Controller restor

--- routes/web.php
+++ routes/web.php
@@ -4,9 +4,7 @@ use App\Http\Controllers\AdminController;
 use App\Http\Controllers\CommentController;
 use App\Http\Controllers\ContactController;
 use App\Http\Controllers\HomeController;
-use App\Http\Controllers\HomesController;
 use App\Http\Controllers\PostController;
-use App\Http\Controllers\TestController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
