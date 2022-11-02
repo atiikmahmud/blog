b79d31601100b60a2d6a8eec197a74fadab1f02a
Atik Mahmud <atikmahmud.cse@gmail.com>
2022-11-02 23:54:34 +0600

    Footer added

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,3 @@
+<footer class="text-center bg-dark text-light py-3" style="min-height: 50px; margin-top: auto;">
+    Copyright © 2022 Data Blog. All rights reserved.
+</footer>
--- resources/views/layouts/layouts.blade.php
+++ resources/views/layouts/layouts.blade.php
@@ -1,48 +1,63 @@
 <!doctype html>
 <html lang="en">
-  <head>
+
+<head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <title>Data Blog - @yield('title')</title>
-    <link rel = "icon" type = "image/png" href = "{{url('/image/fev-icon.png')}}">
+    <link rel="icon" type="image/png" href="{{ url('/image/fev-icon.png') }}">
 
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
 
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.3/css/all.css" integrity="sha384-SZXxX4whJ79/gErwcOYf+zWLeJdY/qpuqC4cAa9rOGUstPomtqpuNWT9wdPEn2fk" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.3/css/all.css"
+        integrity="sha384-SZXxX4whJ79/gErwcOYf+zWLeJdY/qpuqC4cAa9rOGUstPomtqpuNWT9wdPEn2fk" crossorigin="anonymous">
 
     <!-- CKEditor CDN -->
     <script src="https://cdn.ckeditor.com/ckeditor5/23.0.0/classic/ckeditor.js"></script>
 
     <style>
-      .ck-editor__editable{
-        min-height: 185px;
-      }
+        .ck-editor__editable {
+            min-height: 185px;
+        }
     </style>
-    
-  </head>
-  <body>
-    <!-- Navbar begin -->
-    @include('layouts.navbar')
-    <!-- Navbar End -->
 
-    <!-- Body Content Begin -->
-    <div class="main-body-content">
-        @section('content')
-        @show
+</head>
+
+<body>
+    <div style="display: flex; flex-direction: column; min-height: 100vh;">
+        <!-- Navbar Section begin -->
+        @include('layouts.navbar')
+        <!-- Navbar Section End -->
+
+        <!-- Body Content Begin -->
+        <div class="main-body-content" style="margin-bottom: 1.5rem;">
+            @section('content')
+            @show
+        </div>
+        <!-- Body Content End -->
+
+        <!-- Footer Section End -->
+        @include('layouts.footer')
+        <!-- Footer Section End -->
+
+        <script>
+            ClassicEditor
+                .create(document.querySelector('#body'))
+                .catch(error => {
+                    console.error(error);
+                });
+        </script>
+
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js"
+            integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" crossorigin="anonymous">
+        </script>
+
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
+            integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous">
+        </script>
     </div>
-    <!-- Body Content End -->
+</body>
 
-    <script>
-      ClassicEditor
-          .create( document.querySelector( '#body' ) )
-          .catch( error => {
-              console.error( error );
-          } );
-    </script>
-
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" crossorigin="anonymous"></script>
-
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-  </body>
 </html>
